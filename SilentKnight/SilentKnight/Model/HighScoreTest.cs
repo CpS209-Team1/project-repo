08d300e8e982d2773b531ea7cb099957cdeb3642
lutyputy <andrewavinante@gmail.com>
2018-04-06 12:24:14 -0400

    Added Comments

--- SilentKnight/SilentKnight/Model/HighScoreTest.cs
+++ SilentKnight/SilentKnight/Model/HighScoreTest.cs
@@ -1,29 +1,29 @@
-﻿////-----------------------------------------------------------------------------------------------------------------------------------------------------------
-////File:   HighScoreTest.cs
-////Desc:   This file contains the validation tests (both valid and invalid) for HighScore().
-////-----------------------------------------------------------------------------------------------------------------------------------------------------------
+﻿//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+//File:   HighScoreTest.cs
+//Desc:   This file contains the validation tests (both valid and invalid) for HighScore().
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
 
-//namespace SilentKnight.Model
-//{
-//    [TestClass]
-//    class HighScoreTest
-//    {
-//        [TestMethod]
-//        public void ReadScores_LoadScores_Success()
-//        {
-//            HighScore highscores = new HighScore();
-//            highscores.LoadScores("HighScoresTestData.txt");
-//            Assert.IsTrue(highscores.scoreList[0].Name == "Susie");
-//        }
+namespace SilentKnight.Model
+{
+    [TestClass]
+    class HighScoreTest
+    {
+        [TestMethod]
+        public void ReadScores_LoadScores_Success()
+        {
+            HighScore highscores = new HighScore();
+            highscores.LoadScores("HighScoresTestData.txt");
+            Assert.IsTrue(highscores.scoreList[0].Name == "Susie");
+        }
 
-//        [TestMethod]
-//        public void HighScore_ScoreSaved_Success()
-//        {
-//        }
-//    }
-//}
+        [TestMethod]
+        public void HighScore_ScoreSaved_Success()
+        {
+        }
+    }
+}
 
 

5272d6b380e6c0ec742315d218518badd95cffcd
Duelene <dolson@bju.edu>
2018-04-10 16:55:54 -0400

    Added High Scores screen

---
+++ SilentKnight/SilentKnight/HighScoresScreen.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SilentKnight
+{
+    /// <summary>
+    /// Interaction logic for HelpScreen.xaml
+    /// </summary>
+    public partial class HighScoresScreen : Page
+    {
+        public HighScoresScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}

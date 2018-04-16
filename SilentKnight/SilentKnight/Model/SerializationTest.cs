789eda296a5a5daafe4e3491733a35911216b2d2
MBRUN138 <mbrun138@students.bju.edu>
2018-04-16 19:51:51 -0400

    added serialization unit test

--- SilentKnight/SilentKnight/Model/SerializationTest.cs
+++ SilentKnight/SilentKnight/Model/SerializationTest.cs
@@ -12,19 +12,22 @@ namespace SilentKnight.Model
     public class SerializationTest
     {
         [TestMethod]
-        public void Test_LoadGame()
+        public void Test_SaveGame_Then_LoadGame()
         {
             GameController ctrl = new GameController();
             Player.Instance.Login("Mike");
-            ctrl.Load("data.txt");
             World.Instance.Entities.Clear();
             Skeleton ent1 = new Skeleton(Spawn.Instance.observer, 2, 4, "cat.png",4);
             Skeleton ent2 = new Skeleton(Spawn.Instance.observer, 2, 4, "bat.png",6);
             World.Instance.AddEntity(ent1);
             World.Instance.AddEntity(ent2);
             ctrl.Save("data.txt");
-            ctrl.Print();
+            Assert.IsTrue(World.Instance.Entities.Count == 2);
             World.Instance.Entities.Clear();
+            Assert.IsTrue(World.Instance.Entities.Count == 0);
+            ctrl.Load("data.txt");
+            Assert.IsTrue(World.Instance.Entities.Count == 2);
+            Assert.IsTrue(Player.Instance.PlayerName == "Mike");
         }
 
     }

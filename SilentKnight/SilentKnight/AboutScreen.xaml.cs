4f028cee64a380280a7634b650cab048a06792d1
Duelene <dolson@bju.edu>
2018-04-19 00:04:34 -0400

    Screen changes

--- SilentKnight/SilentKnight/AboutScreen.xaml.cs
+++ SilentKnight/SilentKnight/AboutScreen.xaml.cs
@@ -32,6 +32,15 @@ namespace SilentKnight
         {
 
         }
+        private void Windows_Loaded(object sender, RoutedEventArgs e)
+        {
+            AboutText.Text = "You open your eyes. \n" +
+                             "You are disoriented and you don't remember where you are or why you're here.\n";
+//Then a sound...a rustling in the shadows.
+//Footsteps are approaching.You spot the familiar glint of armor emerge from the darkness.
+//One, two, three, and more blades point menacingly in your direction. A blade of light manifests in your hands. Now you remember.
+//You're a knight and you know no fear."
+        }
 
         private void Back_Click(object sender, RoutedEventArgs e)
         {

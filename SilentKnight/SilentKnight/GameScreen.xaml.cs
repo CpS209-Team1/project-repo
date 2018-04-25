56db3065420d0fb6cc0a1dea78443d97099bf52d
lutyputy <andrewavinante@gmail.com>
2018-04-25 18:29:36 -0400

    Fixed Level Num Bug

--- SilentKnight/SilentKnight/GameScreen.xaml.cs
+++ SilentKnight/SilentKnight/GameScreen.xaml.cs
@@ -67,7 +67,7 @@ namespace SilentKnight
             LoadTime();
             scoreNum.Text = "0";
             enemyNum.Text = Convert.ToString(World.Instance.Entities.Count);
-            levelNum.Text = "1";
+            levelNum.Text = Convert.ToString(World.Instance.LevelCount);
             PlayerHealth();
             gameScreen.Width = mw.ActualWidth;
             if (World.Instance.Load == true)
